4bea2a595fbbc818fa31ab04d4458bc4e12055f8
Hung Son <tranhungson.hn2004@gmail.com>
2025-11-06 23:12:18 +0700

    TranHungSon firstcommit

---
+++ Assets/Scripts/Controllers/BoardController.cs
@@ -0,0 +1,474 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    public event Action OnMoveEvent = delegate { };
+
+    public bool IsBusy { get; private set; }
+
+    private Board m_board;
+
+    private GameManager m_gameManager;
+
+    private bool m_isDragging;
+
+    private Camera m_cam;
+
+    private Collider2D m_hitCollider;
+
+    private GameSettings m_gameSettings;
+
+    private List<Cell> m_potentialMatch;
+
+    private float m_timeAfterFill;
+
+    private bool m_hintIsShown;
+
+    private bool m_gameOver;
+
+    [SerializeField] private Transform bottomContainer;
+    private List<Item> bottomItems = new List<Item>();
+    private int maxBottomSlots = 5;
+
+
+    public void StartGame(GameManager gameManager, GameSettings gameSettings)
+    {
+        m_gameManager = gameManager;
+
+        m_gameSettings = gameSettings;
+
+        m_gameManager.StateChangedAction += OnGameStateChange;
+
+        m_cam = Camera.main;
+
+        m_board = new Board(this.transform, gameSettings);
+        if (bottomContainer == null)
+        {
+            GameObject bcObj = GameObject.Find("BottomContainer");
+            if (bcObj != null)
+            {
+                bottomContainer = bcObj.transform;
+                Debug.Log("Found BottomContainer automatically");
+            }
+            else
+            {
+                Debug.LogError(" BottomContainer not found in Scene! Please create one named 'BottomContainer'");
+            }
+        }
+
+        if (bottomContainer != null)
+        {
+            maxBottomSlots = bottomContainer.childCount;
+        }
+
+
+        Fill();
+    }
+
+    private void Fill()
+    {
+        m_board.Fill();
+        FindMatchesAndCollapse();
+    }
+
+    private void OnGameStateChange(GameManager.eStateGame state)
+    {
+        switch (state)
+        {
+            case GameManager.eStateGame.GAME_STARTED:
+                IsBusy = false;
+                break;
+            case GameManager.eStateGame.PAUSE:
+                IsBusy = true;
+                break;
+            case GameManager.eStateGame.GAME_OVER:
+                m_gameOver = true;
+                StopHints();
+                break;
+        }
+    }
+
+
+    /* public void Update()
+     {
+         if (m_gameOver) return;
+         if (IsBusy) return;
+
+         if (!m_hintIsShown)
+         {
+             m_timeAfterFill += Time.deltaTime;
+             if (m_timeAfterFill > m_gameSettings.TimeForHint)
+             {
+                 m_timeAfterFill = 0f;
+                 ShowHint();
+             }
+         }
+
+         if (Input.GetMouseButtonDown(0))
+         {
+             var hit = Physics2D.Raycast(m_cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+             if (hit.collider != null)
+             {
+                 m_isDragging = true;
+                 m_hitCollider = hit.collider;
+             }
+         }
+
+         if (Input.GetMouseButtonUp(0))
+         {
+             ResetRayCast();
+         }
+
+         if (Input.GetMouseButton(0) && m_isDragging)
+         {
+             var hit = Physics2D.Raycast(m_cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+             if (hit.collider != null)
+             {
+                 if (m_hitCollider != null && m_hitCollider != hit.collider)
+                 {
+                     StopHints();
+
+                     Cell c1 = m_hitCollider.GetComponent<Cell>();
+                     Cell c2 = hit.collider.GetComponent<Cell>();
+                     if (AreItemsNeighbor(c1, c2))
+                     {
+                         IsBusy = true;
+                         SetSortingLayer(c1, c2);
+                         m_board.Swap(c1, c2, () =>
+                         {
+                             FindMatchesAndCollapse(c1, c2);
+                         });
+
+                         ResetRayCast();
+                     }
+                 }
+             }
+             else
+             {
+                 ResetRayCast();
+             }
+         }
+     }*/
+    public void Update()
+    {
+        if (m_gameOver) return;
+        if (IsBusy) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.Raycast(m_cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                Cell clickedCell = hit.collider.GetComponent<Cell>();
+                if (clickedCell != null && clickedCell.Item != null)
+                {
+                    MoveItemToBottom(clickedCell);
+                }
+            }
+        }
+    }
+    private void MoveItemToBottom(Cell sourceCell)
+    {
+        if (bottomItems.Count >= maxBottomSlots)
+        {
+            GameOver();
+            return;
+        }
+
+        Item item = sourceCell.Item;
+        if (item == null || item.View == null)
+        {
+            Debug.LogError("Item hoặc Item.View đang NULL!");
+            return;
+        }
+
+        
+        IsBusy = true;
+
+        sourceCell.Free();
+
+        Vector3 targetPos = bottomContainer.GetChild(bottomItems.Count).position;
+        item.View.DOMove(targetPos, 0.4f)
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() =>
+            {
+                item.View.SetParent(bottomContainer);
+                bottomItems.Add(item);
+                CheckForTripleMatch();
+                StartCoroutine(DelayUnlock(0.2f));
+            });
+    }
+
+    private IEnumerator DelayUnlock(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        IsBusy = false;
+    }
+
+
+    private void CheckForTripleMatch()
+    {
+        Dictionary<string, List<Item>> groups = new Dictionary<string, List<Item>>();
+
+        foreach (var item in bottomItems)
+        {
+            string id = item.ID;
+            if (!groups.ContainsKey(id))
+                groups[id] = new List<Item>();
+
+            groups[id].Add(item);
+        }
+
+        foreach (var group in groups.Values)
+        {
+            if (group.Count == 3)
+            {
+                foreach (var i in group)
+                {
+                    bottomItems.Remove(i);
+                    Destroy(i.gameObject);
+                }
+                ReorderBottomSlots();
+                break;
+            }
+        }
+
+        if (IsBoardEmpty()) Win();
+
+    }
+    public void ClearBottomSlots()
+    {
+        // Xóa danh sách item trong code
+        foreach (var item in bottomItems)
+        {
+            if (item != null && item.View != null)
+                Destroy(item.View.gameObject);
+        }
+
+        bottomItems.Clear();
+
+        // Nếu trong bottomContainer vẫn còn object con, xóa luôn
+        for (int i = 0; i < bottomContainer.childCount; i++)
+        {
+            Transform child = bottomContainer.GetChild(i);
+            if (child.childCount > 0)
+            {
+                foreach (Transform t in child)
+                {
+                    Destroy(t.gameObject);
+                }
+            }
+        }
+
+        Debug.Log(" Cleared all bottom slots!");
+    }
+
+    private void ReorderBottomSlots()
+    {
+        for (int i = 0; i < bottomItems.Count; i++)
+        {
+            bottomItems[i].transform.DOMove(bottomContainer.GetChild(i).position, 0.2f);
+        }
+    }
+
+    private bool IsBoardEmpty()
+    {
+        return m_board.AllCellsEmpty(); 
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("YOU LOSE");
+        ClearBottomSlots();
+        m_gameManager.SetState(GameManager.eStateGame.GAME_OVER);
+      
+    }
+
+    private void Win()
+    {
+        Debug.Log("YOU WIN");
+        ClearBottomSlots() ;
+        m_gameManager.SetState(GameManager.eStateGame.WIN);
+    }
+
+
+
+
+    private void ResetRayCast()
+    {
+        m_isDragging = false;
+        m_hitCollider = null;
+    }
+
+    private void FindMatchesAndCollapse(Cell cell1, Cell cell2)
+    {
+        if (cell1.Item is BonusItem)
+        {
+            cell1.ExplodeItem();
+            StartCoroutine(ShiftDownItemsCoroutine());
+        }
+        else if (cell2.Item is BonusItem)
+        {
+            cell2.ExplodeItem();
+            StartCoroutine(ShiftDownItemsCoroutine());
+        }
+        else
+        {
+            List<Cell> cells1 = GetMatches(cell1);
+            List<Cell> cells2 = GetMatches(cell2);
+
+            List<Cell> matches = new List<Cell>();
+            matches.AddRange(cells1);
+            matches.AddRange(cells2);
+            matches = matches.Distinct().ToList();
+
+            if (matches.Count < m_gameSettings.MatchesMin)
+            {
+                m_board.Swap(cell1, cell2, () =>
+                {
+                    IsBusy = false;
+                });
+            }
+            else
+            {
+                OnMoveEvent();
+
+                CollapseMatches(matches, cell2);
+            }
+        }
+    }
+
+    private void FindMatchesAndCollapse()
+    {
+        List<Cell> matches = m_board.FindFirstMatch();
+
+        if (matches.Count > 0)
+        {
+            CollapseMatches(matches, null);
+        }
+        else
+        {
+            m_potentialMatch = m_board.GetPotentialMatches();
+            if (m_potentialMatch.Count > 0)
+            {
+                IsBusy = false;
+
+                m_timeAfterFill = 0f;
+            }
+            else
+            {
+                //StartCoroutine(RefillBoardCoroutine());
+                StartCoroutine(ShuffleBoardCoroutine());
+            }
+        }
+    }
+
+    private List<Cell> GetMatches(Cell cell)
+    {
+        List<Cell> listHor = m_board.GetHorizontalMatches(cell);
+        if (listHor.Count < m_gameSettings.MatchesMin)
+        {
+            listHor.Clear();
+        }
+
+        List<Cell> listVert = m_board.GetVerticalMatches(cell);
+        if (listVert.Count < m_gameSettings.MatchesMin)
+        {
+            listVert.Clear();
+        }
+
+        return listHor.Concat(listVert).Distinct().ToList();
+    }
+
+    private void CollapseMatches(List<Cell> matches, Cell cellEnd)
+    {
+        for (int i = 0; i < matches.Count; i++)
+        {
+            matches[i].ExplodeItem();
+        }
+
+        if(matches.Count > m_gameSettings.MatchesMin)
+        {
+            m_board.ConvertNormalToBonus(matches, cellEnd);
+        }
+
+        StartCoroutine(ShiftDownItemsCoroutine());
+    }
+
+    private IEnumerator ShiftDownItemsCoroutine()
+    {
+        m_board.ShiftDownItems();
+
+        yield return new WaitForSeconds(0.2f);
+
+        m_board.FillGapsWithNewItems();
+
+        yield return new WaitForSeconds(0.2f);
+
+        FindMatchesAndCollapse();
+    }
+
+    private IEnumerator RefillBoardCoroutine()
+    {
+        m_board.ExplodeAllItems();
+
+        yield return new WaitForSeconds(0.2f);
+
+        m_board.Fill();
+
+        yield return new WaitForSeconds(0.2f);
+
+        FindMatchesAndCollapse();
+    }
+
+    private IEnumerator ShuffleBoardCoroutine()
+    {
+        m_board.Shuffle();
+
+        yield return new WaitForSeconds(0.3f);
+
+        FindMatchesAndCollapse();
+    }
+
+
+    private void SetSortingLayer(Cell cell1, Cell cell2)
+    {
+        if (cell1.Item != null) cell1.Item.SetSortingLayerHigher();
+        if (cell2.Item != null) cell2.Item.SetSortingLayerLower();
+    }
+
+    private bool AreItemsNeighbor(Cell cell1, Cell cell2)
+    {
+        return cell1.IsNeighbour(cell2);
+    }
+
+    internal void Clear()
+    {
+        m_board.Clear();
+    }
+
+    private void ShowHint()
+    {
+        m_hintIsShown = true;
+        foreach (var cell in m_potentialMatch)
+        {
+            cell.AnimateItemForHint();
+        }
+    }
+
+    private void StopHints()
+    {
+        m_hintIsShown = false;
+        foreach (var cell in m_potentialMatch)
+        {
+            cell.StopHintAnimation();
+        }
+
+        m_potentialMatch.Clear();
+    }
+}
